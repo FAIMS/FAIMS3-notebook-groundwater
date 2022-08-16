a233f90219143f235d34d07c13925a8e7df4310f
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-08-16 11:45:18 +1000

    loaded to prod
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- loadGroundwater.sh
+++ loadGroundwater.sh
@@ -2,9 +2,9 @@
 
 set -euo pipefail
 
-export SERVER_FULLNAME="${1:-demo.db.faims.edu.au}" # dev, alpha, testing, selenium
+export SERVER_FULLNAME="${1:-prod.db.faims.edu.au}" # dev, alpha, testing, selenium
 cp groundwater.json faims3-temp-notebook-migrator/testnotebook.json
 cp groundwater.json faims3-temp-notebook-migrator/
 cd faims3-temp-notebook-migrator
 echo "Load Groundwater ${SERVER_FULLNAME}"
-bash loadNotebook.sh $SERVER_FULLNAME "Macquarie Groundwater Notebook $(date)" "groundwater.json" "groundwater_$(uuid)"
+bash loadNotebook.sh $SERVER_FULLNAME "Macquarie Groundwater -- Vegetation Notebook September 2022" "groundwater.json" "groundwater"
